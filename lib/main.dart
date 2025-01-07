033f3f9d212653c8f500850318ff2c388aabecaf
hitesh3634804 <hitesh@lifesparktech.com>
2025-01-07 22:28:42 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => FavouriteBloc(FavouriteRepository())..add(FetchFavouriteList())),
+        ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        themeMode: ThemeMode.dark,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          useMaterial3: true,
+        ),
+        home: const FavouriteAppScreen(),
+      ),
+    );
+  }
+}
